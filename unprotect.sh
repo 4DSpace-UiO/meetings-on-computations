14a2ee7ea1600839b4166fd0f6113439290e3911
sigvaldm <sigvald@marebakken.com>
2021-06-29 10:35:37 +0200

    Added protected slides to internal page

--- unprotect.sh
+++ unprotect.sh
@@ -4,10 +4,15 @@
 
 mkdir -p unprotected
 cd protected
+
 for file in *.enc
 do
     base=$(basename "$file" .enc)
     openssl enc -d -aes256 -pbkdf2 -pass "pass:$1" -in "$file" -out "../unprotected/$base"
 done
 
-
+for file in *.7z
+do
+    base=$(basename "$file" .7z)
+    7z x -o"../unprotected/" -p"$1" "$file" 
+done
